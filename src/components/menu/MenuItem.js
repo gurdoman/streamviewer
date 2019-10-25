56caaf4ef4778794e7670f433cd6717ec4fc82c4
Adrián Rossino <gurdoman@hotmail.com>
2019-10-25 07:19:47 -0500

    Added google buttons

--- src/components/menu/MenuItem.js
+++ src/components/menu/MenuItem.js
@@ -1,10 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { GoogleLogin } from 'react-google-login';
 
 export default class MenuItem extends Component {
   render() {
+
+    const responseGoogle = (response) => {
+      console.log(response);
+    }
+    
     return (
       <li className="header-nav_item">
-        Login
+        <GoogleLogin
+          clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+          render={renderProps => (
+            <span onClick={renderProps.onClick} disabled={renderProps.disabled}>Login</span>
+          )}
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        />
       </li>
     )
   }
