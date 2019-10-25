ffb13b01b368bf21970a77d787a1abd01189a500
Adrián Rossino <gurdoman@hotmail.com>
2019-10-25 17:35:34 -0500

    Updates

---
+++ precache-manifest.5ae13da0af2ea27a7a3ec71058bd42f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bee9dd2fbb7482a44dee1ffa31a5593",
+    "url": "/streamviewer/index.html"
+  },
+  {
+    "revision": "8ccb048c52a0783a0825",
+    "url": "/streamviewer/static/css/main.aad5e35d.chunk.css"
+  },
+  {
+    "revision": "34e08e584fe629fdf54d",
+    "url": "/streamviewer/static/js/2.819a6ea4.chunk.js"
+  },
+  {
+    "revision": "8ccb048c52a0783a0825",
+    "url": "/streamviewer/static/js/main.e684fce5.chunk.js"
+  },
+  {
+    "revision": "39e3acb94a0939367616",
+    "url": "/streamviewer/static/js/runtime-main.df3b8fac.js"
+  },
+  {
+    "revision": "291d5f1a1843985fcb384888e62633ee",
+    "url": "/streamviewer/static/media/logo.291d5f1a.svg"
+  }
+]);
