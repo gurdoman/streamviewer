616837ce189b62bb001ec61ca4744590ca44bbaf
Adrián Rossino <gurdoman@hotmail.com>
2019-11-01 06:10:53 -0600

    Fixed bug with dates

--- src/components/youtube/OtherVideo.js
+++ src/components/youtube/OtherVideo.js
@@ -3,13 +3,14 @@ import React, { Component } from 'react'
 export default class OtherVideo extends Component {
   render() {
     const {id, publishedAt, thumbnailUrl, title} = this.props.video;
-
+    let date = new Date(parseInt(publishedAt)).toGMTString();
+    console.log(date)
     return (
       <div className="other-video" onClick={this.props.changeVideo.bind(this, this.props.video)}>
         <img className="other-video_image" src={thumbnailUrl} alt={id}/>
         <div className="other-video_description">
           <span className="other-video_title">{title}</span>
-          <span className="other-video_date">{Date(publishedAt)}</span>
+          <span className="other-video_date">{date}</span>
         </div>
       </div>
     )
